cf41c69ec354363ff1ae4bf84fc28664255cad0e
Mauro Longano <mauro.longano@marconirovereto.it>
2024-12-31 23:46:47 +0000

    feat: add cors to /api

--- src/wrangler.ts
+++ src/wrangler.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+app.use("/api/**", cors());
 
 app.get("/api/posts/:slug/comments", async (c) => {
   // Do something and return an HTTP response
